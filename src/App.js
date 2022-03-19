311d24776931e15294421c1d4bc2e6162c62d466
Igor Coutinho <igoralcoutinho@gmail.com>
2022-03-19 17:59:06 -0300

    creating project and test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,98 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useState, useEffect } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const itemsApi = () =>
+  new Promise((resolve, reject) => {
+    // useLoadind('false')
+
+    setTimeout(() => {
+      resolve([
+        { userId: 1, name: "Orange" },
+        { userId: 2, name: "Banana" },
+        { userId: 1, name: "Strawberry" },
+        { userId: 1, name: "Apple" },
+        { userId: 3, name: "Orange" },
+        { userId: 2, name: "Apple" },
+      ]);
+    }, 2000);
+  });
+
+const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [data, setData] = useState([
+    { userId: 1, name: "Orange" },
+    { userId: 2, name: "Banana" },
+    { userId: 1, name: "Strawberry" },
+    { userId: 1, name: "Apple" },
+    { userId: 3, name: "Orange" },
+    { userId: 2, name: "Apple" },
+  ]);
+  const [amosMoses] = useState("Amos moses");
+  const [isDataReady, setIsDataReady] = useState(false);
+
+  // itemsApi().then((result ) => {
+  //     setData(result);
+  //   }
+  // );
+
+  useEffect(() => {
+    setIsLoading(false);
+    changeData();
+  }, []);
+
+  const changeData = () => {
+    const list = data.reduce((object, current) => {
+      if (object[current.userId]) {
+        object[current.userId] = [...object[current.userId], current.name];
+      } else {
+        object[current.userId] = [current.name];
+      }
+
+      return object;
+    }, {});
+
+    setData(list);
+    setIsDataReady(true);
+  };
+  const returnList = () => {
+    console.log("data", data);
+
+    if (data) {
+      return (
+        <div>
+          {Object.entries(data).map(([key, value]) => (
+            <div>
+              <ul key={key}>
+                UserId: {key}
+                {value.map((item, index) => (
+                  <li key={index}>{item}</li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
+      );
+    } else {
+      <></>;
+    }
+  };
+
+  if (isLoading) {
+    return (
+      <div>
+        <ul>
+          <li>Loading</li>
+        </ul>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <div>{isDataReady && returnList()}</div>
+      </div>
+    );
+  }
+};
 
 export default App;
